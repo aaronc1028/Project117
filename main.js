7202fc82ae79e802f9a3c17f1c29a6735de4e8d7
aaronc1028 <81634761+aaronc1028@users.noreply.github.com>
2022-01-04 20:01:03 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(300,300)
+canvas.center()
+background("white")
+
+}
+
+function draw(){
+
+}
+
+function clearCanvas(){
+    background("white")
+}
